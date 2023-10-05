155760111a7877853c7d5544a92d12d9d3b972c4
J-Skarbek <john@snarkmediaagency.com>
2023-10-05 14:35:49 -0500

    Add a couple of basic link elements

--- vanLife/src/App.jsx
+++ vanLife/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<TestAboutComponent />}/>
