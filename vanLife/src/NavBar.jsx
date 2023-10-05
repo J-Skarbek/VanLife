6b0566eed0d17ae9e54dc9d1a05547e30b7c142e
J-Skarbek <john@snarkmediaagency.com>
2023-10-05 17:26:46 -0500

    Flesh out the navBar component and adjust existant css as needed

--- vanLife/src/NavBar.jsx
+++ vanLife/src/NavBar.jsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="nav-bar">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+    <div className="nav-container bg-tan w-screen flex justify-center">
+      <div className="nav-bar p-4 w-screen max-w-7xl flex justify-between items-center align-center">
+        <div className="logo">
+          <h1 className="font-black uppercase">&#35;VanLife</h1>
+        </div>
+        <nav className="font-semibold capitalize flex w-1/12 justify-between">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </div>
     </div>
   )
 }
--- vanLife/tailwind.config.js
+++ vanLife/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tan: '#FFF7ED'
+      }
+    },
   },
   plugins: [],
 }
