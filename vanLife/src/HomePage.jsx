406117474ba49bce0740ea02a2331bfe518b341d
J-Skarbek <john@snarkmediaagency.com>
2023-10-06 17:06:23 -0500

    Remove the old hardcoded button and make a couple final touchups for now on the homepage

--- vanLife/src/HomePage.jsx
+++ vanLife/src/HomePage.jsx
@@ -5,9 +5,8 @@ function HomePage() {
   return (
     <div className="main flex justify-center bg-home-hero bg-center bg-no-repeat bg-cover min-h-screen">
       <div className="container pl-4 py-8 pr-56 w-screen max-w-7xl flex flex-col justify-start text-white">
-        <h1 className="font-extrabold text-4xl">You got the travel plans,<br></br>we got the travel vans.</h1>
-        <p className="font-medium text-xl">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        <button type="button">Find Your Van</button>
+        <h1 className="pb-8 font-extrabold text-4xl">You got the travel plans,<br></br>we got the travel vans.</h1>
+        <p className="pb-8 font-medium text-xl">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
         <Button text={'Find Your Van'}/>
       </div>
     </div>
