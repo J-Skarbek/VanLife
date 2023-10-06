cf4a5908288f321fe7a89c61c22ded31c516c21a
J-Skarbek <john@snarkmediaagency.com>
2023-10-06 16:56:33 -0500

    More stylings for the hompage component

--- vanLife/src/HomePage.jsx
+++ vanLife/src/HomePage.jsx
@@ -3,9 +3,10 @@ import React from "react";
 function HomePage() {
   return (
     <div className="main flex justify-center bg-home-hero bg-center bg-no-repeat bg-cover min-h-screen">
-      <div className="container p-4 w-screen max-w-7xl flex flex-col justify-between items-center align-center">
-        <h1 className="font-extrabold text-4xl">You got the travel plans, we got the travel vans.</h1>
+      <div className="container pl-4 py-8 pr-56 w-screen max-w-7xl flex flex-col justify-start text-white">
+        <h1 className="font-extrabold text-4xl">You got the travel plans,<br></br>we got the travel vans.</h1>
         <p className="font-medium text-xl">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <button type="button">Find Your Van</button>
       </div>
     </div>
   )
