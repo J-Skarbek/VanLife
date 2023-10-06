2c6caab5e59b5c37f14f9cd8ea4af3af82a0286c
J-Skarbek <john@snarkmediaagency.com>
2023-10-06 16:20:42 -0500

    Start fleshing out the content on the homepage component

--- vanLife/src/HomePage.jsx
+++ vanLife/src/HomePage.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="main flex justify-center bg-home-hero">
-      <div className="container p-4 w-screen max-w-7xl flex justify-between items-center align-center">
-        <h1>This is a test 'Home' Compoenent</h1>
+    <div className="main flex justify-center bg-home-hero bg-center bg-no-repeat bg-cover min-h-screen">
+      <div className="container p-4 w-screen max-w-7xl flex flex-col justify-between items-center align-center">
+        <h1 className="font-extrabold text-4xl">You got the travel plans, we got the travel vans.</h1>
+        <p className="font-medium text-xl">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
       </div>
     </div>
   )
