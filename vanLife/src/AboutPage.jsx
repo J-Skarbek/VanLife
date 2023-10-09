c4c38ba9f1711e4549e0f3921733c01bc75f2334
J-Skarbek <john@snarkmediaagency.com>
2023-10-09 12:11:47 -0500

    Work on alignment for the about page divs

--- vanLife/src/AboutPage.jsx
+++ vanLife/src/AboutPage.jsx
@@ -3,19 +3,17 @@ import aboutHero from "/public/hero-images/aboutPage-heroImg.jpg"
 
 function AboutPage() {
   return (
-    <div className="main flex flex-col justify-center">
-      <div className="container p-4 w-screen max-w-7xl flex justify-between items-center align-center">
-        <div className="heroImg flex justify-center bg-about-hero">
-          {/* <img src={aboutHero} alt="About the Van Life" /> */}
-          <div className="spacer w-screen h-96"></div>
+    <div className="main flex justify-center pb-4 w-screen max-w-7x min-h-screen">
+      <div className="flex flex-col pb-4 w-screen max-w-7xl">
+        <div className="heroImg mb-12 w-screen max-w-7xl bg-about-hero bg-center bg-no-repeat bg-cover">
+            <div className="spacer w-screen max-w-7xl h-96"></div>
         </div>
-
-      </div>
-      <div className="pageContent">
-        <h1 className="text-3xl font-bold">Don’t squeeze in a sedan when you could relax in a van.</h1>
-        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
-        (Hitch costs extra &#128521;).</p>
-        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+        <div className="pageContent p-4">
+          <h1 className="text-3xl font-bold">Don’t squeeze in a sedan when you could relax in a van.</h1>
+          <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+          (Hitch costs extra &#128521;)</p>
+          <p className="py-8">Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+        </div>
       </div>
     </div>
   )
