154d1ddbe2a31e02b0ca5dd457f4a570aa2571ae
J-Skarbek <john@snarkmediaagency.com>
2023-10-11 18:10:57 -0500

    Test the mapping of vans and rednering

--- vanLife/src/pages/VanListingPage.jsx
+++ vanLife/src/pages/VanListingPage.jsx
@@ -2,20 +2,29 @@ import React from "react";
 
 function VanListings() {
 
-  const [vanListings, setVanListings] = React.useState(null);
+  const [vans, setVans] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api/vans", { mode: "cors" })
       .then((response) => response.json())
-      .then((response) => setVanListings(response))
-      .then((response) => console.log(response))
-      .then((response) => console.log(vanListings))
+      .then((data) => setVans(data.vans))
       .catch((error) => console.error(error));
   }, []);
+
+  // console.log(vans[0]);
+
+  const vanElements = vans.map(van => {
+    return (
+      <div key={van.id}>
+        <h3>{van.name}</h3>
+      </div>
+    )
+  })
 
   return (
     <>
       <h1>This will be the van listing page.</h1>
+      {vanElements}
     </>
   )
 }
