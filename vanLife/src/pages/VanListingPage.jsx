16bf6295cc694e12826e39ea70b6432f303688b6
J-Skarbek <john@snarkmediaagency.com>
2023-10-11 17:04:49 -0500

    Create a fetch request to pull in the van data

--- vanLife/src/pages/VanListingPage.jsx
+++ vanLife/src/pages/VanListingPage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
 function VanListings() {
+
+  const [vanListings, setVanListings] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/vans", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => setVanListings(response))
+      .then((response) => console.log(response))
+      .then((response) => console.log(vanListings))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <h1>This will be the van listing page.</h1>
