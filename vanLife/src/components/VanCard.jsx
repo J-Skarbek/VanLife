1fe2913d35537825d7330e8d2c1b1d10068dd7ed
J-Skarbek <john@snarkmediaagency.com>
2023-10-12 11:33:24 -0500

    Flesh out the vanCard component

--- vanLife/src/components/VanCard.jsx
+++ vanLife/src/components/VanCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Button from "./Button";
 
 function VanCard(props) {
 
   return (
-    <div>
-      <h3>{props.name}</h3>
+    <div className="van-card flex flex-col w-80">
       <img src={props.imageUrl} alt={props.name} />
+      <div className="van-details flex justify-between">
+        <h2>{props.name}</h2>
+        <p className="van-rates">$100/day</p> 
+        {/* Change the rate to use a prop for the rate */}
+      </div>
+      <Button 
+        text="Book your Van"
+        // Will need to pass more props and link values in eventually
+      />
     </div>
   )
 }
