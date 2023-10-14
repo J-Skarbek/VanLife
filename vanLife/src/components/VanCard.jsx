5e10e2f46b9f88a384bfc22f2f618d5df33c591d
J-Skarbek <john@snarkmediaagency.com>
2023-10-14 11:20:37 -0500

    More styling changes to the vanlistings page and van card components

--- vanLife/src/components/VanCard.jsx
+++ vanLife/src/components/VanCard.jsx
@@ -6,15 +6,14 @@ function VanCard(props) {
   return (
     <div className="van-card flex flex-col w-80 basis-5/12 m-4">
       <img src={props.imageUrl} alt={props.name} className="object-cover rounded-xl" />
-      <div className="van-details flex justify-between text-black">
+      <div className="van-details flex justify-between text-xl text-black font-semibold my-2">
         <h2>{props.name}</h2>
-        <p className="van-rates">$100/day</p> 
-        {/* Change the rate to use a prop for the rate */}
+        <p className="van-rates">${props.price}<br></br>/day</p> 
       </div>
       <Button 
         text="Book your Van"
         color="brandOrange"
-        classes="w-1/2"
+        classes="w-1/2 my-2"
         // Will need to pass more props and link values in eventually
       />
     </div>
--- vanLife/src/pages/VanListingPage.jsx
+++ vanLife/src/pages/VanListingPage.jsx
@@ -20,34 +20,39 @@ function VanListings() {
         key={van.id}
         name={van.name}
         imageUrl={van.imageUrl}
+        price={van.price}
+        type={van.type}
+        description={van.description}
       />
     )
   })
+
+  console.log(vans)
 
   return (
     <div className="main flex justify-center">
       <div className="container pl-4 py-8 xl:pr-56 w-screen max-w-7xl flex flex-col justify-start text-white">
         <h1 className="text-black font-bold mb-4">Explore Our Van Options</h1>
-        <div className="van-filters-container flex">
+        <div className="van-filters-container flex my-4">
           <Button 
             text="Simple"
             color="brandOrange"
-            classes="w-1/2"
+            classes="w-1/2 mx-2"
           />
           <Button 
             text="Rugged"
             color="brandOrange"
-            classes="w-1/2"
+            classes="w-1/2 mx-2"
           />
           <Button 
             text="Luxury"
             color="brandOrange"
-            classes="w-1/2"
+            classes="w-1/2 mx-2"
           />
           <Button 
             text="Clear Filters"
             // color="brandBlack"
-            classes="w-1/2 text-black"
+            classes="w-1/2 text-black mx-2"
           />
         </div>
         <div className="vansGrid grid grid-cols-2 auto-rows-auto w-5/6">
