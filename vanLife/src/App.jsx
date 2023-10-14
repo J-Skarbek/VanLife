b86bbc7360dfe5478367d4e63f607378aac3b471
J-Skarbek <john@snarkmediaagency.com>
2023-10-14 14:11:53 -0500

    Rename and add header tags to navbar and move footer into components folder

--- vanLife/src/App.jsx
+++ vanLife/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar';
+import Header from './components/Header';
 import HomePage from './pages//HomePage';
 import AboutPage from './pages/AboutPage';
 import VanListings from './pages/VanListingPage';
 import VanDetails from './pages/VanDetails';
-import Footer from './Footer';
+import Footer from './components/Footer';
 import './App.css'
 
 import "./server"
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/about" element={<AboutPage />}/>
---
+++ vanLife/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <div className="nav-container bg-tan w-screen flex justify-center">
+        <div className="nav-bar p-4 w-screen max-w-7xl flex justify-between items-center align-center">
+          <div className="logo">
+            <h1 className="font-black uppercase">&#35;VanLife</h1>
+          </div>
+          <nav className="font-semibold capitalize flex w-36 justify-between">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/van-listings">Vans</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
