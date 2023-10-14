2a688bff827a8312a00b46dbc9f09f92da2f3920
J-Skarbek <john@snarkmediaagency.com>
2023-10-14 12:54:23 -0500

    Pull in the van data on details page and save it to state

--- vanLife/src/pages/VanDetails.jsx
+++ vanLife/src/pages/VanDetails.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function VanDetails() {
+
+  const params = useParams();
+  const [van, setVan] = React.useState(null);
+  console.log(params)
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`, { mode: "cors" })
+      .then((response) => response.json())
+      .then((vanData) => setVan(vanData.vans))
+      .catch((error) => console.error(error));
+  }, [params.id]);
+
   return (
-    <p className="test">This is the vandetails template.</p>
+    <div className="main flex justify-center pb-4 w-screen max-w-7x min-h-screen">
+      <div className="flex flex-col pb-4 w-screen max-w-7xl">
+      </div>
+    </div>
   )
 }
 
