639be8911f78f88091b2e596a43c60a8ee020e88
J-Skarbek <john@snarkmediaagency.com>
2023-10-14 15:17:40 -0500

    Start with nesting some of the host routes

--- vanLife/src/App.jsx
+++ vanLife/src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from './pages//HomePage';
 import AboutPage from './pages/AboutPage';
 import VanListings from './pages/vans/VanListingPage';
 import VanDetails from './pages/Vans/VanDetails';
-import Host from './pages/Host/Host';
+import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import './App.css'
@@ -22,9 +22,12 @@ function App() {
           <Route path="/about" element={<AboutPage />}/>
           <Route path="/van-listings" element={<VanListings />}/>
           <Route path="/van-listings/:id" element={<VanDetails />}/>
-          <Route path="/host" element={<Host />}/>
-          <Route path="/host/income" element={<Income />}/>
-          <Route path="/host/reviews" element={<Reviews />}/>
+          <Route path="/host" element={<Dashboard />}>
+             <Route path="/host/income" element={<Income />}/>
+             <Route path="/host/reviews" element={<Reviews />}/>
+          </Route>
+          {/* <Route path="/host/income" element={<Income />}/>
+          <Route path="/host/reviews" element={<Reviews />}/> */}
         </Route>
       </Routes>
     </BrowserRouter>
--- vanLife/src/pages/Host/Dashboard.jsx
+++ vanLife/src/pages/Host/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function Dashboard() {
   return(
     <>
       <h1>This is the Dashboard Route.</h1>
+      <Outlet />
     </>
   )
 }
