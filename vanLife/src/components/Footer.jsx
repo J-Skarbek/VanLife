190536ae4380c64baef02fb2bab74bb4ee66f816
J-Skarbek <john@snarkmediaagency.com>
2023-10-16 11:38:48 -0500

    Fix the footer symbol

--- vanLife/src/components/Footer.jsx
+++ vanLife/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer flex justify-center align-center items-center bg-offBlack h-28">
-    <p className="text-offGray font-medium text-xl">&#64;2023 | #VANLIFE</p>
+    <p className="text-offGray font-medium text-xl">&#169;2023 | #VANLIFE</p>
   </div>
   )
 }
