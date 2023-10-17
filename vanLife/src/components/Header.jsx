1a2aea35208d98f77d7efe89c49ac8d7c1598df7
J-Skarbek <john@snarkmediaagency.com>
2023-10-17 10:15:40 -0500

    Testing active classes and styling with navlinks

--- vanLife/src/components/Header.jsx
+++ vanLife/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
@@ -10,10 +10,35 @@ function Header() {
             <h1 className="font-black uppercase">&#35;VanLife</h1>
           </div>
           <nav className="font-semibold capitalize flex w-36 justify-between">
-            <Link to="/" className="mx-2">Home</Link>
-            <Link to="/about" className="mx-2">About</Link>
-            <Link to="/van-listings" className="mx-2">Vans</Link>
-            <Link to="/host" className="mx-2">Host</Link>
+            <NavLink 
+              to="/" 
+              // className={`mx-2 ${({isActive}) => isActive ? "italic" : ""}`}
+              // className="mx-2"
+              className={({isActive}) => isActive ? "italic" : ""}
+            >
+              Home
+            </NavLink>
+            <NavLink 
+              to="about" 
+              // className="mx-2"
+              className={({isActive}) => isActive ? "italic" : ""}
+            >
+              About
+            </NavLink>
+            <NavLink 
+              to="van-listings" 
+              // className="mx-2"
+              className={({isActive}) => isActive ? "italic" : ""}
+            >
+              Vans
+            </NavLink>
+            <NavLink 
+              to="host" 
+              // className="mx-2"
+              className={({isActive}) => isActive ? "italic" : ""}
+            >
+              Host
+            </NavLink>
           </nav>
         </div>
       </div>
