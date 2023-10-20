5a92c616c5301a197d5e655962192ad944903cfd
J-Skarbek <john@snarkmediaagency.com>
2023-10-20 18:03:43 -0500

    Add link styles to these nav links

--- vanLife/src/pages/Host/HostDashBoardSubnavigation.jsx
+++ vanLife/src/pages/Host/HostDashBoardSubnavigation.jsx
@@ -3,25 +3,32 @@ import { NavLink } from "react-router-dom";
 
 function HostDashBoardSubnavigation() {
 
+  const customLinkStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#16161"
+  }
+
   return (
     <>
       <nav>
+      <NavLink 
+          to="."
+          end
+          className="mx-2"
+          style={({isActive}) => isActive ? customLinkStyles : null}        
+        >Details
+        </NavLink>
         <NavLink 
           to="pricing" 
           className="mx-2"
-          
+          style={({isActive}) => isActive ? customLinkStyles : null}         
         >Pricing
-        </NavLink>
-        <NavLink 
-          to="."
-          className="mx-2"
-          
-        >Details
         </NavLink>
         <NavLink 
           to="photos" 
           className="mx-2"
-          
+          style={({isActive}) => isActive ? customLinkStyles : null}         
         >Photos
         </NavLink>
       </nav>
