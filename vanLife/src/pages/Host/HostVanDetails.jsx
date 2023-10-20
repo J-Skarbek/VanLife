256f7f336a92aa70ffd006094afa1945dacbad7b
J-Skarbek <john@snarkmediaagency.com>
2023-10-20 15:18:53 -0500

    Adjusting the flex layout

--- vanLife/src/pages/Host/HostVanDetails.jsx
+++ vanLife/src/pages/Host/HostVanDetails.jsx
@@ -16,16 +16,19 @@ function HostVanDetails() {
 
   return (
     <>
-      <div className="main flex justify-center">
+      <div className="main flex justify-center bg-tan">
         <div className="container pl-4 py-8 xl:pr-56 w-screen max-w-7xl flex flex-col justify-start">
           {
              hostVanDetails ? (
-              <div className="van-details">
-                <img src={`${hostVanDetails.imageUrl}`} alt={hostVanDetails.name} className="mb-12 w-screen h-3/4 xl:h-2/4 object-cover max-w-7xl"/>
-                <p className="van-type">{hostVanDetails.type}</p>
-                <h2 className="bold text-2xl">{hostVanDetails.name}</h2>
-                <p className="description">{hostVanDetails.description}</p>
-                <button className="link-button">Rent this van</button>
+              <div className="van-details flex">
+                <div className="image-container w-1/5">
+                  <img src={`${hostVanDetails.imageUrl}`} alt={hostVanDetails.name} className="cover"/>
+                </div>
+                <div className="host-van-info flex flex-col justify-center px-4">
+                  <span className="van-type">{hostVanDetails.type}</span>
+                  <h2 className="font-bold text-4xl my-4">{hostVanDetails.name}</h2>
+                  <p className="van-price font-semibold">{`$${hostVanDetails.price}/day`}</p>
+                </div>
               </div>
             ) : <h2 className="loading">Loading...</h2> 
           }
