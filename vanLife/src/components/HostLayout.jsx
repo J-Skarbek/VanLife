f3609318d4ddf068dc4c23066dfe1db47864f86b
J-Skarbek <john@snarkmediaagency.com>
2023-10-20 12:09:03 -0500

    Add host van listings page and start on fixiing some styling there and in other files

--- vanLife/src/components/HostLayout.jsx
+++ vanLife/src/components/HostLayout.jsx
@@ -10,44 +10,46 @@ function HostLayout() {
   }
 
   return (
-    <>
-      <nav className="host-dashboard-naviatgation">
-        <NavLink 
-          to="/host" 
-          className="mx-2"
-          end
-          style={({isActive}) => isActive ? customLinkStyles : null}
-        >
-          Dashboard
-        </NavLink>
-        <NavLink 
-          to="/host/income" 
-          className="mx-2"
-          // end
-          style={({isActive}) => isActive ? customLinkStyles : null}
-          
-        >
-          Income
-        </NavLink>
-        <NavLink 
-          to="/host/reviews" 
-          className="mx-2"
-          // end
-          style={({isActive}) => isActive ? customLinkStyles : null}
-        >
-          Reviews
-        </NavLink>
-        <NavLink 
-          to="/host/vans" 
-          className="mx-2"
-          // end
-          style={({isActive}) => isActive ? customLinkStyles : null}
-        >
-          Vans
-        </NavLink>
-      </nav>
-      <Outlet />
-    </>
+    <div className="main flex justify-center">
+      <div className="container pl-4 py-8 xl:pr-56 w-screen max-w-7xl flex flex-col justify-start text-white">
+        <nav className="host-dashboard-naviatgation">
+          <NavLink 
+            to="/host" 
+            className="mx-2"
+            end
+            style={({isActive}) => isActive ? customLinkStyles : null}
+          >
+            Dashboard
+          </NavLink>
+          <NavLink 
+            to="/host/income" 
+            className="mx-2"
+            // end
+            style={({isActive}) => isActive ? customLinkStyles : null}
+            
+          >
+            Income
+          </NavLink>
+          <NavLink 
+            to="/host/reviews" 
+            className="mx-2"
+            // end
+            style={({isActive}) => isActive ? customLinkStyles : null}
+          >
+            Reviews
+          </NavLink>
+          <NavLink 
+            to="/host/vans" 
+            className="mx-2"
+            // end
+            style={({isActive}) => isActive ? customLinkStyles : null}
+          >
+            Vans
+          </NavLink>
+        </nav>
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
