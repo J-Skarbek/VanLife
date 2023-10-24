e63ea4789d2b6f64d6ececd41b4d791ba57c93db
J-Skarbek <john@snarkmediaagency.com>
2023-10-24 11:34:30 -0500

    Add links using paramaters

--- vanLife/src/pages/Vans/VanListings.jsx
+++ vanLife/src/pages/Vans/VanListings.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import VanCard from "../../components/VanCard";
 import Button from "../../components/Button";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 
 function VanListings() {
 
@@ -43,7 +43,27 @@ function VanListings() {
       <div className="container pl-4 py-8 xl:pr-56 w-screen max-w-7xl flex flex-col justify-start text-white">
         <h1 className="text-black font-bold mb-4">Explore Our Van Options</h1>
         <div className="van-filters-container flex my-4">
-          <Button 
+          <Link 
+            to="?type=simple"
+            className="w-1/2 mx-2"          
+          >Simple
+          </Link>
+          <Link 
+            to="?type=rugged"
+            className="w-1/2 mx-2"          
+          >Rugged
+          </Link>
+          <Link 
+            to="?type=luxury" 
+            className="w-1/2 mx-2"                    
+          >Luxury
+          </Link>
+          <Link 
+            to="." 
+            className="w-1/2 mx-2"                  
+          >All Vans
+          </Link>
+          {/* <Button 
             text="Simple"
             color="brandOrange"
             classes="w-1/2 mx-2"
@@ -62,7 +82,7 @@ function VanListings() {
             text="Clear Filters"
             // color="brandBlack"
             classes="w-1/2 text-black mx-2"
-          />
+          /> */}
         </div>
         <div className="vansGrid grid grid-cols-2 auto-rows-auto w-5/6">
         {vanElements}
