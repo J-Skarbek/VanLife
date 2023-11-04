b5e0e41c01bd3bbda4f0b97865f11a58e98018ab
J-Skarbek <john@snarkmediaagency.com>
2023-11-04 18:07:42 -0500

    Create Error details

--- vanLife/src/errorElements/VansRouteError.jsx
+++ vanLife/src/errorElements/VansRouteError.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 function VansRouteError() {
+  const error = useRouteError();
+  console.log(error)
   return (
-    'There was an error here.'
+    <>
+      <h1>There was an error here.</h1>
+      <p>{error.message}</p>
+      <p>{error.status}</p>
+      <p>{error.statusText}</p>
+    </>
+
   )
 }
 
