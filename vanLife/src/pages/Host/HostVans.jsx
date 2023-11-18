273ed2d76cd8dd5111f9dd67bbcad6b372b36d70
J-Skarbek <john@snarkmediaagency.com>
2023-11-18 16:00:03 -0600

    Remove unneeded code

--- vanLife/src/pages/Host/HostVans.jsx
+++ vanLife/src/pages/Host/HostVans.jsx
@@ -14,54 +14,8 @@ export async function loader({ request }) {
 
 function HostVans() {
 
-  // const vans = useLoaderData();
   const dataPromise = useLoaderData();
 
-  // const hostVansEls = vans.map(van => (
-  //     <Link
-  //       to={van.id}
-  //       key={van.id}
-  //       className="host-van-link-wrapper"
-  //     >
-  //       <div className="host-van-single flex bg-tan my-4 drop-shadow-md" key={van.id}>
-  //         <div className="image-container w-1/5">
-  //           <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-  //         </div>
-  //         <div className="host-van-info flex flex-col justify-center px-4">
-  //             <h3>{van.name}</h3>
-  //             <p>${van.price}/day</p>
-  //         </div>
-  //       </div>
-  //     </Link>
-  // ))
-
-  // function renderHostVans(vans) {
-  //   const hostVansEls = vans.map(van => {
-  //     return (
-  //       <Link
-  //         to={van.id}
-  //         key={van.id}
-  //         className="host-van-link-wrapper"
-  //       >
-  //         <div className="host-van-single flex bg-tan my-4 drop-shadow-md" key={van.id}>
-  //           <div className="image-container w-1/5">
-  //             <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-  //           </div>
-  //           <div className="host-van-info flex flex-col justify-center px-4">
-  //             <h3>{van.name}</h3>
-  //             <p>${van.price}/day</p>
-  //           </div>
-  //         </div>
-  //       </Link>
-  //     )
-  //   });
-
-  //   return (
-  //     <section>
-  //       {hostVansEls}
-  //     </section>
-  //   )
-  // }
 
   return (
     <section>
