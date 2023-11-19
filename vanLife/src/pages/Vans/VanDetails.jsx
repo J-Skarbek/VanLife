b6cc8d067c8c547c7cc5906df4f7879914698bec
J-Skarbek <john@snarkmediaagency.com>
2023-11-19 15:49:29 -0600

    Start refactor on the standard vans details page

--- vanLife/src/pages/Vans/VanDetails.jsx
+++ vanLife/src/pages/Vans/VanDetails.jsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { Link, useLocation, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { 
+  Link, 
+  useLocation, 
+  useLoaderData,
+  defer,
+  Await
+} from "react-router-dom";
 import Button from "../../components/Button";
 import { getVans } from "../../api";
 
 export function loader({ params }) {
-  return getVans(params.id)
+  // return getVans(params.id)
+  return defer({ van: getVans(params.id) })
 }
 
 function VanDetails() {
-
   const location = useLocation();
-  const van  = useLoaderData();
+  const dataPromise  = useLoaderData();
 
   const search = location.state?.search || "";
   const type = location.state?.type || "all";
@@ -24,15 +30,23 @@ function VanDetails() {
           className="back-button"
         >&larr;<span>Back to {type} vans</span>
         </Link>
-        {van ? (
-          <div className="van-details">
-            <img src={`${van.imageUrl}`} alt={van.name} className="mb-12 w-screen h-3/4 xl:h-2/4 object-cover max-w-7xl"/>
-            <p className="van-type">{van.type}</p>
-            <h2 className="bold text-2xl">{van.name}</h2>
-            <p className="description">{van.description}</p>
-            <button className="link-button">Rent this van</button>
-          </div>
-        ) : <h2 className="loading">Loading...</h2> }
+        <Suspense fallback={<h2 className="text-black">Loading Van...</h2>}>
+          <Await resolve={dataPromise.van}>
+            {van => {
+              return (
+              van ? (
+                <div className="van-details">
+                  <img src={`${van.imageUrl}`} alt={van.name} className="mb-12 w-screen h-3/4 xl:h-2/4 object-cover max-w-7xl"/>
+                  <p className="van-type">{van.type}</p>
+                  <h2 className="bold text-2xl">{van.name}</h2>
+                  <p className="description">{van.description}</p>
+                  <button className="link-button">Rent this van</button>
+                </div>
+              ) : <h2 className="loading">Loading...</h2>
+              )
+            }}
+          </Await>
+        </Suspense>
       </div>
     </div>
   )
