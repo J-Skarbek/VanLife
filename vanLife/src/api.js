1c1dfbc58acb1235b09036222ebdb5df9fa74281
J-Skarbek <john@snarkmediaagency.com>
2023-11-20 11:18:53 -0600

    Refactor the api.js file to use firebase

--- vanLife/src/api.js
+++ vanLife/src/api.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRprpue35fX3qMYnC_BpAbB7eiK6VDh54",
+  authDomain: "vanlife-553f3.firebaseapp.com",
+  projectId: "vanlife-553f3",
+  storageBucket: "vanlife-553f3.appspot.com",
+  messagingSenderId: "26762075776",
+  appId: "1:26762075776:web:dd504d46ad34d33dac5065"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Refactoring the fetching functions
+
 export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : "/api/vans"
   const res = await fetch(url)
